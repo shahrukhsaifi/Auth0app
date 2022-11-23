46a1efbf2fa6277a142a16a5ee9a03fcce7ff2c2
Shahrukh Saifi <Shahrukhsaifi36@gmail.com>
2022-11-23 21:46:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {useState} from 'react'
+const initialState=()=>{
+  return 10;
 }
+function App(){
+  const [temperatureValue, settemperatureValue] = useState(initialState())
+  const [tempratureColor, setTempraturColor] = useState('cold')
 
+  const increment=()=>{
+  settemperatureValue(prevCount=>prevCount+1)
+  if(temperatureValue===20){
+    return settemperatureValue(temperatureValue)
+  }
+    if (temperatureValue>=15){
+      return setTempraturColor('hot')
+    }
+  }
+
+  const decreament=()=>{
+    settemperatureValue(prevCount=>prevCount-1)
+    if(temperatureValue<=10){
+      settemperatureValue(temperatureValue)
+    };
+    if (temperatureValue<=15){
+      return setTempraturColor('cold')
+    } ;
+  }
+
+  const ResetFunction = ()=>{
+        settemperatureValue(10)
+      return  setTempraturColor('cold')
+  }
+  return(
+    <div className='app-container'>
+			<div className='temperature-display-container'>
+				<div className={`temperature-display ${tempratureColor}`}>{temperatureValue}°C</div>
+			</div>
+			<div className='button-container'>
+				<button onClick={increment}>+</button>
+				<button onClick={decreament}>-</button>
+			</div>
+      <div className='button-container'>
+				<button onClick={ResetFunction}>Reset</button>
+			</div>
+		</div>
+  )
+}
 export default App;
