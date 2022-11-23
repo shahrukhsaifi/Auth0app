70e4638228cadbdc4aa61dc9bcb2d10f636c7697
shahrukhsaifi36@gmail.com <Shahrukhsaifi36@gmail.com>
2022-11-23 22:13:00 +0530

    Quizappfirstcommit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,127 @@
-import React, {useState} from 'react'
-const initialState=()=>{
-  return 10;
-}
-function App(){
-  const [temperatureValue, settemperatureValue] = useState(initialState())
-  const [tempratureColor, setTempraturColor] = useState('cold')
+import React, { useState } from "react";
 
-  const increment=()=>{
-  settemperatureValue(prevCount=>prevCount+1)
-  if(temperatureValue===20){
-    return settemperatureValue(temperatureValue)
-  }
-    if (temperatureValue>=15){
-      return setTempraturColor('hot')
+function App() {
+  // Properties
+  const [showResults, setShowResults] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const questions = [
+    {
+      text: "What is the capital of America?",
+      options: [
+        { id: 0, text: "New York City", isCorrect: false },
+        { id: 1, text: "Boston", isCorrect: false },
+        { id: 2, text: "Santa Fe", isCorrect: false },
+        { id: 3, text: "Washington DC", isCorrect: true },
+      ],
+    },
+    {
+      text: "What year was the Constitution of America written?",
+      options: [
+        { id: 0, text: "1787", isCorrect: true },
+        { id: 1, text: "1776", isCorrect: false },
+        { id: 2, text: "1774", isCorrect: false },
+        { id: 3, text: "1826", isCorrect: false },
+      ],
+    },
+    {
+      text: "Who was the second president of the US?",
+      options: [
+        { id: 0, text: "John Adams", isCorrect: true },
+        { id: 1, text: "Paul Revere", isCorrect: false },
+        { id: 2, text: "Thomas Jefferson", isCorrect: false },
+        { id: 3, text: "Benjamin Franklin", isCorrect: false },
+      ],
+    },
+    {
+      text: "What is the largest state in the US?",
+      options: [
+        { id: 0, text: "California", isCorrect: false },
+        { id: 1, text: "Alaska", isCorrect: true },
+        { id: 2, text: "Texas", isCorrect: false },
+        { id: 3, text: "Montana", isCorrect: false },
+      ],
+    },
+    {
+      text: "Which of the following countries DO NOT border the US?",
+      options: [
+        { id: 0, text: "Canada", isCorrect: false },
+        { id: 1, text: "Russia", isCorrect: true },
+        { id: 2, text: "Cuba", isCorrect: true },
+        { id: 3, text: "Mexico", isCorrect: false },
+      ],
+    },
+  ];
+
+  // Helper Functions
+
+  /* A possible answer was clicked */
+  const optionClicked = (isCorrect) => {
+    // Increment the score
+    if (isCorrect) {
+      setScore(score + 1);
     }
-  }
 
-  const decreament=()=>{
-    settemperatureValue(prevCount=>prevCount-1)
-    if(temperatureValue<=10){
-      settemperatureValue(temperatureValue)
-    };
-    if (temperatureValue<=15){
-      return setTempraturColor('cold')
-    } ;
-  }
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setShowResults(true);
+    }
+  };
 
-  const ResetFunction = ()=>{
-        settemperatureValue(10)
-      return  setTempraturColor('cold')
-  }
-  return(
-    <div className='app-container'>
-			<div className='temperature-display-container'>
-				<div className={`temperature-display ${tempratureColor}`}>{temperatureValue}°C</div>
-			</div>
-			<div className='button-container'>
-				<button onClick={increment}>+</button>
-				<button onClick={decreament}>-</button>
-			</div>
-      <div className='button-container'>
-				<button onClick={ResetFunction}>Reset</button>
-			</div>
-		</div>
-  )
+  /* Resets the game back to default */
+  const restartGame = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setShowResults(false);
+  };
+
+  return (
+    <div className="App">
+      {/* 1. Header  */}
+      <h1>USA Quiz 🇺🇸</h1>
+
+      {/* 2. Current Score  */}
+      <h2>Score: {score}</h2>
+
+      {/* 3. Show results or show the question game  */}
+      {showResults ? (
+        /* 4. Final Results */
+        <div className="final-results">
+          <h1>Final Results</h1>
+          <h2>
+            {score} out of {questions.length} correct - (
+            {(score / questions.length) * 100}%)
+          </h2>
+          <button onClick={() => restartGame()}>Restart game</button>
+        </div>
+      ) : (
+        /* 5. Question Card  */
+        <div className="question-card">
+          {/* Current Question  */}
+          <h2>
+            Question: {currentQuestion + 1} out of {questions.length}
+          </h2>
+          <h3 className="question-text">{questions[currentQuestion].text}</h3>
+
+          {/* List of possible answers  */}
+          <ul>
+            {questions[currentQuestion].options.map((option) => {
+              return (
+                <li
+                  key={option.id}
+                  onClick={() => optionClicked(option.isCorrect)}
+                >
+                  {option.text}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
+
 export default App;
