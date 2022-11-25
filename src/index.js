2efa2ee567c4e9e928f2bbb9cde599de07b2a22e
shahrukhsaifi36@gmail.com <Shahrukhsaifi36@gmail.com>
2022-11-25 16:26:33 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './style.css';
-
+import { Auth0Provider } from "@auth0/auth0-react";
+// const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+// const clientID = process.env.REACT_APP_AUTH0_CLIENT_ID;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <Auth0Provider
+    domain="dev-j25f3z8aahlbqf64.us.auth0.com"
+    clientId="tpUecxx3DAJruTOk8wE4QHRZIArd5Fv0"
+    redirectUri={window.location.origin}
+  >
     <App />
+  </Auth0Provider>,
 );
 
